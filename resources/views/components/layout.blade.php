9ef9393910c5b04589271f083fbb9d57aa560508
NekroBluster <alberto.comerci91@gmail.com>
2024-10-13 18:08:51 +0200

    Integrato l'offcanvas con all'interno i form di accedi e registrati, e il tasto logout. Modificata la navbar. Tolte le viste di register e login.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,11 +26,89 @@
 
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
         <div class="offcanvas-header">
-            ciao
+            
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-            ciao
+            
+
+
+            @guest
+            <div class="accordion " id="accordionExample">
+                <div class="accordion-item">
+                  <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                      Accedi
+                    </button>
+                  </h2>
+                  <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+        
+                            <div class="mb-3">
+                              <label for="exampleInputEmail1" class="form-label">Email</label>
+                              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+                            
+                            </div>
+                            <div class="mb-3">
+                              <label for="exampleInputPassword1" class="form-label">Password</label>
+                              <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                            </div>
+                          
+                            <button type="submit" class="btn btn-primary">Accedi</button>
+                          </form>
+                    </div>
+                  </div>
+                </div>
+                
+                <div class="accordion-item">
+                  <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Registrati
+                    </button>
+                  </h2>
+                  <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="nome" class="form-label">Nome</label>
+                                <input type="text" class="form-control" id="nome" name="name">
+                                
+                            </div>
+                            
+                            
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+                                
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                            </div>
+                            
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Conferma Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" name="password_confirmation">
+                            </div>
+                            
+                            <button type="submit" class="btn btn-primary">Registrati</button>
+                        </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              @else
+              
+              <form class="dropdown-item" action="{{ route('logout') }}" method="post">@csrf <button
+                class="btn btn-danger" type="submit"> Logout </button> </form>
+
+            @endguest
+
+
         </div>
     </div>
 </body>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('welcome') }}">Presto.it</a>
+        <a class="navbar-brand text-white" href="{{ route('welcome') }}">Presto.it</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,53 +8,40 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('indexArticle') }}">Tutti gli articoli</a>
+                    <a class="nav-link text-white" href="{{ route('indexArticle') }}">Tutti gli articoli</a>
                 </li>
             </ul>
-            @guest
-                <ul class="ms-auto">
-                    <a class="nav-link dropdown-toggle text-black" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false"><i class="fa-regular fa-user"></i></a>
 
-                    <div class="dropdown-menu rounded-3 ms-auto">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1"
-                                    aria-describedby="emailHelp" name="email">
+            @auth
 
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" name="password">
-                            </div>
 
-                            <button type="submit" class="btn btn-primary">Accedi</button>
-                        </form>
-                    </div>
-                @else
+                <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Benvenuto {{ Auth::user()->name }}</a>
+                        <a class="nav-link text-white" href="{{ route('createArticle') }}">Inserisci un articolo</a>
                     </li>
-                    <li class="nav-item">
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('createArticle') }}">Inserisci un articolo</a>
-                    </li>
-
-                    <form class="dropdown-item" action="{{ route('logout') }}" method="post">@csrf <button
-                            class="btn btn-danger" type="submit"> Logout </button> </form>
+                </ul>
+                <ul class="navbar-nav ms-auto">
+                    {{-- <li class="nav-item">
+                        <a class="nav-link" href="#">{{ Auth::user()->name }}</a>
+                    </li> --}}
                 </ul>
 
-            @endguest
+                <button class="btn my-bg-quar btn-info-custom " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+                    aria-controls="offcanvasRight">{{ Auth::user()->name }} <i class="fa-regular fa-user ms-1"></i></button>
+            @else
+                <button class="btn my-bg-quar btn-info-custom ms-auto" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i
+                        class="fa-regular fa-user"></i></button>
+
+            @endauth
+
+
+
+
 
         </div>
     </div>
-    
-    
-    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
-        aria-controls="offcanvasRight">prova</button>
+
+
+
 </nav>
-
-
