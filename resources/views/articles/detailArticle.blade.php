a51f9c383f5879dd1b167c02fd4be8e9239addbb
ANTONELLA_DEBORA_CALVIO <A.DEBORACALVIO@gmail.com>
2024-10-16 13:01:48 +0200

    inizio modifica della pagina di dettagli e del revisore

--- resources/views/articles/detailArticle.blade.php
+++ resources/views/articles/detailArticle.blade.php
@@ -3,19 +3,42 @@
         <div class="row d-flex justify-content-center">
             
             <div class="col-6 ">
-                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img src="https://mdbootstrap.com/img/new/standard/nature/111.webp" class="img-fluid" />
-                    <a href="#!">
-                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                </div>
+                <div id="carouselExampleIndicators" class="carousel slide ">
+                    <div class="carousel-indicators">
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner rounded">
+                      <div class="carousel-item active">
+                        <img src="https://mdbootstrap.com/img/new/standard/nature/111.webp" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="https://mdbootstrap.com/img/new/standard/nature/111.webp" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="https://mdbootstrap.com/img/new/standard/nature/111.webp" class="d-block w-100" alt="...">
+                      </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
             </div>
-            <div class="col-6">
+            <div class="col-6  d-flex flex-column justify-content-center">
                 <h1>{{ $article->title }}</h1>
-                <p>Prezzo: €{{ $article->price }} </p>
-                <p>Categoria: {{ $article->category->name }}</p>
+
+                <a class="btn my-bg-quar btn-info-custom w-25 mb-5"
+                href="{{ route('byCategory', ['category' => $article->category]) }}">{{ $article->category->name }} <i class="fa-solid fa-circle-right ms-1"></i></a>
+                <h2>€{{ $article->price }} </h2>
                 <p>Inserzionista: {{ $article->user->name }}</p>
                 <p>Data di creazione: {{ $article->created_at->format('d/m/Y') }}</p>
+                <a class="btn my-bg-quar btn-info-custom w-25 mb-5" href="#" > Contatta <i class="fa-solid fa-message ms-1"></i></a>
             </div>
             <div class="col-12 mt-3">
                 <p>Descrizione: {{ $article->description }}</p>
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -30,41 +30,52 @@
                     
                     @endfor
                 </div>
-                <div class="col-md-4">
-                    <div>
-                        <h1>{{$article_to_check->title}}</h1>
-                        <h3>Autore: {{$article_to_check->user->name}}</h3>
-                        <h4>€{{$article_to_check->price}}</h4>
-                        <h4 class="fst-italic text-muted">#{{$article_to_check->category->name}}</h4>
-                        <p>{{$article_to_check->description}}</p>
-                    </div>
-                    <div>
-                        <form action="{{ route('rejectArticle', ['article' => $article_to_check]) }}" method="POST">
-                            @csrf
-                            @method('PATCH')
-                            
-                            <button type="submit" class="btn btn-danger">Rifiuta</button>
-                        </form>
-                        
-                        <form action="{{ route('acceptArticle', ['article' => $article_to_check]) }}" method="POST">
-                            @csrf
-                            @method('PATCH')
-                            
-                            <button type="submit" class="btn btn-success">Approva</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            @else
-            <div class="row">
-                <div class="col-12">
-                    <h1>Nessun articolo da revisionare</h1>
-                    <a href="{{ route('welcome') }}" class="btn btn-primary">Torna alla home</a>
-                </div>
-            </div>
-            @endif
         </div>
-        
-        
-        
-    </x-layout>
+        <div class="col-4  d-flex flex-column justify-content-center">
+            <h1>{{ $article_to_check->title }}</h1>
+            
+            <a class="btn my-bg-quar btn-info-custom w-25 mb-5"
+            href="{{ route('byCategory', ['category' => $article_to_check->category]) }}">{{ $article_to_check->category->name }} <i class="fa-solid fa-circle-right ms-1"></i></a>
+            <h2>€{{ $article_to_check->price }} </h2>
+            <p>Inserzionista: {{ $article_to_check->user->name }}</p>
+            <p>Data di creazione: {{ $article_to_check->created_at->format('d/m/Y') }}</p>
+            <a class="btn my-bg-quar btn-info-custom w-25 mb-5" href="#" > Contatta <i class="fa-solid fa-message ms-1"></i></a>
+        </div>
+        <div class="col-12 mt-3">
+            <p>Descrizione: {{ $article_to_check->description }}</p>
+        </div>
+
+                
+                
+                
+            
+                <div>
+                    <form action="{{ route('rejectArticle', ['article' => $article_to_check]) }}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        
+                        <button type="submit" class="btn btn-danger">Rifiuta</button>
+                    </form>
+                    
+                    <form action="{{ route('acceptArticle', ['article' => $article_to_check]) }}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        
+                        <button type="submit" class="btn btn-success">Approva</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        @else
+        <div class="row">
+            <div class="col-12">
+                <h1>Nessun articolo da revisionare</h1>
+                <a href="{{ route('welcome') }}" class="btn btn-primary">Torna alla home</a>
+            </div>
+        </div>
+        @endif
+    </div>
+    
+    
+    
+</x-layout>
