8c8758564e1d947e1cd5c19f075265e0256a0b4d
DavideFattori <fattorid5@gmail.com>
2024-10-16 15:15:01 +0200

    modificata la vista dell'offcanvas quando l'utente è loggato, aggiunto il pallino di notifica per l'utente revisore anche nella sezione dell'offcanvas

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,98 +18,121 @@
 </head>
 
 <body>
-  <x-navbar/>
-  {{ $slot }}
-  <x-footer />
+    <x-navbar />
+    {{ $slot }}
+    <x-footer />
 
 
-  {{-- offcanvas con form di login/register --}}
-  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-      <div class="offcanvas-header">
-          
-          <button type="button" class="my-bg-quar btn-info-custom btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-          
-          @guest
-          <div class="accordion" id="accordionExample">
-              <div class="accordion-item my-bg-sec">
-                <h2 class="accordion-header">
-                  <button class="accordion-button my-bg-main text-white collapsed btn-accordion-custom" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Accedi
-                  </button>
-                </h2>
-                <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                  <div class="accordion-body">
-                      <form method="POST" action="{{ route('login') }}">
-                          @csrf
-      
-                          <div class="mb-3 container-custom">
-                            
-                            <input placeholder="Email" type="email" class="input-custom w-100 py-2" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
-                          </div>
+    {{-- offcanvas con form di login/register --}}
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+        <div class="offcanvas-header">
 
-                          <div class="mb-3 container-custom">
-                            
-                            <input placeholder="Password" type="password" class="input-custom w-100 py-2" id="exampleInputPassword1" name="password">
-                          </div>
-                        
-                          <button type="submit" class="btn btn-info-custom my-bg-quar">Accedi</button>
-                        </form>
-                  </div>
+            <button type="button" class="my-bg-quar btn-info-custom btn-close" data-bs-dismiss="offcanvas"
+                aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+
+            @guest
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item my-bg-sec">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button my-bg-main text-white collapsed btn-accordion-custom"
+                                type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
+                                aria-controls="collapseOne">
+                                Accedi
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+
+                                    <div class="mb-3 container-custom">
+
+                                        <input placeholder="Email" type="email" class="input-custom w-100 py-2"
+                                            id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+                                    </div>
+
+                                    <div class="mb-3 container-custom">
+
+                                        <input placeholder="Password" type="password" class="input-custom w-100 py-2"
+                                            id="exampleInputPassword1" name="password">
+                                    </div>
+
+                                    <button type="submit" class="btn btn-info-custom my-bg-quar">Accedi</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item my-bg-sec">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button my-bg-main text-white collapsed btn-accordion-custom"
+                                type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree"
+                                aria-expanded="false" aria-controls="collapseThree">
+                                Registrati
+                            </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <form method="POST" action="{{ route('register') }}">
+                                    @csrf
+                                    <div class="mb-3 container-custom">
+                                        <input placeholder="Nome" type="text" class="input-custom w-100 py-2"
+                                            id="nome" name="name">
+                                    </div>
+                                    <div class="mb-3 container-custom">
+                                        <input placeholder="Email" type="email" class="input-custom w-100 py-2"
+                                            id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+                                    </div>
+                                    <div class="mb-3 container-custom">
+                                        <input placeholder="Password" type="password" class="input-custom w-100 py-2"
+                                            id="exampleInputPassword1" name="password">
+                                    </div>
+                                    <div class="mb-3 container-custom">
+                                        <input placeholder="Conferma Password" type="password"
+                                            class="input-custom w-100 py-2" id="exampleInputPassword1"
+                                            name="password_confirmation">
+                                    </div>
+
+                                    <button type="submit" class="btn btn-info-custom my-bg-quar">Registrati</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              
-              <div class="accordion-item my-bg-sec">
-                <h2 class="accordion-header">
-                  <button class="accordion-button my-bg-main text-white collapsed btn-accordion-custom" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                      Registrati
-                  </button>
-                </h2>
-                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                  <div class="accordion-body">
-                      <form method="POST" action="{{ route('register') }}">
-                          @csrf
-                          <div class="mb-3 container-custom">
-                              <input placeholder="Nome" type="text" class="input-custom w-100 py-2" id="nome" name="name">
-                          </div>
-                          <div class="mb-3 container-custom">
-                              <input placeholder="Email" type="email" class="input-custom w-100 py-2" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
-                          </div>
-                          <div class="mb-3 container-custom">
-                              <input placeholder="Password" type="password" class="input-custom w-100 py-2" id="exampleInputPassword1" name="password">
-                          </div>
-                          <div class="mb-3 container-custom">
-                              <input placeholder="Conferma Password" type="password" class="input-custom w-100 py-2" id="exampleInputPassword1" name="password_confirmation">
-                          </div>
-                          
-                          <button type="submit" class="btn btn-info-custom my-bg-quar">Registrati</button>
-                      </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-
             @else
-            @if (Auth::user()->is_revisor)
-                    <li class="nav-item">
-                        <a href="{{ route('indexRevisor') }}" class="nav-link text-white position position-relative w-sm-25">Zona Revisore</a>
-                    </li>
-                @endif
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                  <a class="nav-link text-white" href="{{ route('createArticle') }}">Inserisci un articolo</a>
-              </li>
-            </ul>
-            
-            <form class="dropdown-item" action="{{ route('logout') }}" method="post">@csrf <button
-              class="btn btn-danger" type="submit"> Logout </button> </form>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 border rounded p-0">
+                          <div class="w-100">
+                            <a class="nav-link text-white text-center border-bottom py-3 my-bg-main btn-offcanvas-logged rounded-top" href="{{ route('createArticle') }}">Inserisci un articolo</a>
 
-          @endguest
+                            @if (Auth::user()->is_revisor)
+                              @if (\App\Models\Article::toBeRevisedCount() > 0)
+                                <span class="notifica-revisore-offcanvas translate-middle badge rounded-pill bg-danger">
+                                  {{ \App\Models\Article::toBeRevisedCount() }}
+                                </span>
+                              @endif
+                                <a href="{{ route('indexRevisor') }}" class="nav-link text-white my-bg-main position position-relative w-sm-25 text-center py-3 border-bottom btn-offcanvas-logged">Zona Revisore</a>
+                            @endif
+
+                          </div>
+
+                            <form class="" action="{{ route('logout') }}" method="post">
+                              @csrf 
+                              <button class="rounded-bottom py-2 bg-danger text-white border-0 w-100" type="submit"> Logout </button> 
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+
+            @endguest
 
 
-      </div>
-  </div>
+        </div>
+    </div>
 </body>
 
 </html>
